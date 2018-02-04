6f0dc7fbd3cdad0b13363b13ef43e27d41a26f84
Makoto Tanabe <xekid78@outlook.com>
2018-02-04 12:03:31 +0900

    インターフェースを実装したローカルクラス

---
+++ Sample44.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Sample44 {
+
+	public static void main(String[] args) {
+		class Calc5 implements Add4, Multiply3{
+		    public void method(String str) {
+		        System.out.println(str + "をします");
+		    }
+
+		    public void add() {
+		    	System.out.println(Add4.A + " + " + Add4.B + " は " + (Add4.A + Add4.B) + " です。");
+		    }
+
+		    public void mul() {
+		    	System.out.println(Multiply3.A + " x " + Multiply3.B + " は " + (Multiply3.A * Multiply3.B) + " です。");
+		    }
+		}
+
+		Calc5 calc = new Calc5();
+        calc.method("足し算");
+        calc.add();
+        calc.method("掛け算");
+        calc.mul();
+
+	}
+
+}
+
+interface Add4 {
+	public int A = 7;
+	public int B = 8;
+
+	void method(String str);
+    void add();
+}
+
+interface Multiply3 {
+	public int A = 4;
+	public int B = 5;
+
+	void method(String str);
+    void mul();
+}
